497010ac75164658affa1987ab4107f5af0dee9a
dalbino <danhe178@student.liu.se>
2020-02-21 15:26:32 +0100

    add initial chainlink functions

--- contracts/Trip.sol
+++ contracts/Trip.sol
@@ -58,16 +58,27 @@ contract Trip {
     bool public isRefundable;
     bool public isActive;
 
+    bytes32 constant JOB_ID_GET_PATH = bytes32("76ca51361e4e444f8a9b18ae350a5725"); //same as using stringToBytes32("76ca51361e4e444f8a9b18ae350a5725")
+    bytes32 constant JOB_ID_POST_PATH = bytes32("897479ba429445e4a89be90cfcd52a51");
+    uint256 constant private ORACLE_PAYMENT = 1 * LINK;
+
+    bytes32 public timeAtLocation;
+
     modifier restricted() {
         require(managers[msg.sender]);
         _;
     }
 
+    event RequestTimeAtLocation(
+        bytes32 indexed requestId,
+        bytes32 indexed time
+    );
+
     function addManager(address newManagerAddress) public restricted {
         managers[newManagerAddress] = true;
     }
 
-    constructor(address manager, uint _price, bool _isActive, string memory _trainID, string memory _advertisedTimeAtLocation, string memory _locationSignature) public {
+    constructor(address manager, uint _price, bool _isActive, string memory _trainID, string memory _advertisedTimeAtLocation, string memory _locationSignature, address _oracle) public {
         managers[manager] = true;
         price = _price;
         isActive = _isActive;
@@ -75,6 +86,10 @@ contract Trip {
         advertisedTimeAtLocation = _advertisedTimeAtLocation;
         locationSignature = _locationSignature;
         passengerCount = 0;
+
+        setPublicChainlinkToken();
+    
+        setChainlinkOracle(_oracle);
     }
 
     function bookTrip() public payable{
@@ -113,8 +128,25 @@ contract Trip {
 
     //function refund() public {}
 
-    //function requestTimeAtLocation() public {}
+    function requestTimeAtLocation() public restricted {
+        Chainlink.Request memory req = buildChainlinkRequest(JOB_ID_GET_PATH, this, this.fulfillTimeAtLocation.selector);
+        req.add("get", "https://pastebin.com/raw/MNvNvVQ3"); //This is a test-url. It should be exchanged with 'https://api.trafikinfo.trafikverket.se/v2/data.json'.
+        req.add("path", "RESPONSE.RESULT.0.TrainAnnouncement.0.TimeAtLocation");
+        sendChainlinkRequest(req, ORACLE_PAYMENT);
+    }
 
-    //function fulfillTimeAtLocation() public {}
+    function fulfillTimeAtLocation(bytes32 _requestId, bytes32 _time) public recordChainlinkFulfillment(_requestId) {
+        emit RequestTimeAtLocation(_requestId, _time);
+        timeAtLocation = _time;
+    }
 
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function withdrawLink() public restricted {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+    }
+    
 }
