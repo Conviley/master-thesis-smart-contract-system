2cd64aada35ff3919722c7387afa78f89df6eabf
Tjelvar Guo <=>
2020-02-26 14:36:45 +0100

    overhaul system design

--- contracts/Trip.sol
+++ contracts/Trip.sol
@@ -1,84 +1,45 @@
-pragma solidity ^0.6.2;
+pragma solidity ^0.5.1;
+
+import "./HitchensUnorderedKeySet.sol";
 
 import "chainlink/v0.6/contracts/ChainlinkClient.sol"; // Comment out this line when testing in remix
 
-// import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/ChainlinkClient.sol"; // Uncomment this line when testing in remix
+//import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.5/ChainlinkClient.sol"; // Uncomment this line when testing in remix
 
-contract TripFactory {
-    address[] public trips;
-    mapping(address => bool) public managers;
+contract DeRail is ChainlinkClient{
+    using HitchensUnorderedKeySetLib for HitchensUnorderedKeySetLib.Set;
+    HitchensUnorderedKeySetLib.Set tripSet;
+    
+    struct Trip {
+        uint tripID;
+        uint passengerCount;
+        uint paybackRatio;
+        uint price;
+        string trainID;
+        string locationSignature;
+        string advertisedTimeAtLocation;
+        bytes32 timeAtLocation;
+        mapping(address => uint) passengers;
+        bool isRefundable;
+        bool isActive;
 
-    address constant CHAINLINK_ORACLE_ROPSTEN = 0xc99B3D447826532722E41bc36e644ba3479E4365;
-
+    }
+    
     modifier restricted() {
-        require(managers[msg.sender]);
+        require(managers[msg.sender], "msg.sender is not a manager!");
+        _;
+    }
+    
+    modifier requireTrip(uint key) {
+        require(tripSet.exists(key), "Can't get a widget that doesn't exist.");
         _;
     }
 
-    constructor() public {
-        managers[msg.sender] = true;
-    }
-
-    function addManager(address newManagerAddress) public restricted {
-        managers[newManagerAddress] = true;
-    }
-
-    function createMockTrip() public restricted {
-        trips.push(
-            address(
-                new Trip(
-                    msg.sender,
-                    10000,
-                    true,
-                    "545",
-                    "2020-02-18",
-                    "Nr",
-                    CHAINLINK_ORACLE_ROPSTEN
-                )
-            )
-        );
-    }
-
-    function createTrip(
-        uint256 price,
-        bool isActive,
-        string memory trainID,
-        string memory advertisedTimeAtLocation,
-        string memory locationSignature,
-        address oracle
-    ) public restricted {
-        trips.push(
-            address(
-                new Trip(
-                    msg.sender,
-                    price,
-                    isActive,
-                    trainID,
-                    advertisedTimeAtLocation,
-                    locationSignature,
-                    oracle
-                )
-            )
-        );
-    }
-
-    function getTrips() public view returns (address[] memory) {
-        return trips;
-    }
-}
-
-contract Trip is ChainlinkClient {
-    uint256 public passengerCount;
-    uint256 public paybackRatio;
-    uint256 public price;
-    string public trainID;
-    string public locationSignature;
-    string public advertisedTimeAtLocation;
-    mapping(address => uint256) public passengers;
     mapping(address => bool) public managers;
-    bool public isRefundable;
-    bool public isActive;
-
+    mapping(uint => Trip) public trips;
+    
+    address constant CHAINLINK_ORACLE_ROPSTEN = 0xc99B3D447826532722E41bc36e644ba3479E4365;
+    
     bytes32 constant JOB_ID_GET_PATH = bytes32(
         "76ca51361e4e444f8a9b18ae350a5725"
     ); //same as using stringToBytes32("76ca51361e4e444f8a9b18ae350a5725")
@@ -88,90 +49,103 @@ contract Trip is ChainlinkClient {
     bytes32 constant JOB_ID_ALARM_CLOCK = bytes32(
         "2ebb1c1a4b1e4229adac24ee0b5f784f"
     );
-    uint256 private constant ORACLE_PAYMENT = 1 * LINK;
+    uint private constant ORACLE_PAYMENT = 1 * LINK;
 
-    bytes32 public timeAtLocation;
-
-    modifier restricted() {
-        require(managers[msg.sender]);
-        _;
-    }
-
+    uint public activeTripKey;
+    
+    event LogNewTrip(
+        address sender,
+        uint key,
+        uint tripID,
+        uint passengerCount,
+        uint paybackRatio,
+        uint price,
+        string trainID,
+        string locationSignature,
+        string advertisedTimeAtLocation,
+        bytes32 timeAtLocation,
+        bool isRefundable,
+        bool isActive);
+    event LogUpdateTripPrice(address sender, uint key, uint price);    
+    event LogRemTrip(address sender, uint key);
+    event LogNewTripPassenger(address passengerAddressr, uint key, uint price);
+    
     event RequestAlarmClock(bytes32 indexed requestId);
-
     event RequestTimeAtLocation(
         bytes32 indexed requestId,
         bytes32 indexed time
     );
-
-    function addManager(address newManagerAddress) public restricted {
+    
+    constructor() public {
+        managers[msg.sender] = true;
+        setPublicChainlinkToken();
+    }
+    
+    function addManager(address newManagerAddress) external restricted {
         managers[newManagerAddress] = true;
     }
-
-    constructor(
-        address manager,
-        uint256 _price,
-        bool _isActive,
-        string memory _trainID,
-        string memory _advertisedTimeAtLocation,
-        string memory _locationSignature,
-        address _oracle
-    ) public {
-        managers[manager] = true;
-        price = _price;
-        isActive = _isActive;
-        trainID = _trainID;
-        advertisedTimeAtLocation = _advertisedTimeAtLocation;
-        locationSignature = _locationSignature;
-        passengerCount = 0;
-
-        setPublicChainlinkToken();
-
-        setChainlinkOracle(_oracle);
-
+    
+    function createMockTripHitch() external restricted{
+        uint key = getTripCount();
+        Trip memory newTrip = Trip({
+            tripID: key,
+            passengerCount: 0,
+            paybackRatio: 0,
+            price: 1 ether,
+            trainID: "545",
+            locationSignature: "Nr",
+            advertisedTimeAtLocation: "2020-02-18",
+            timeAtLocation: 0x0,
+            isRefundable: false,
+            isActive: true
+        });
+        tripSet.insert(key);
+        trips[key] = newTrip;
+        
+        setChainlinkOracle(CHAINLINK_ORACLE_ROPSTEN);
+        emit LogNewTrip(msg.sender, key, key, 0, 0, 10000, "545", "Nr", "2020-02-18",0x0, false, true);
     }
-
-    function bookTrip() public payable {
-        require(msg.value == price);
-        passengers[msg.sender] = price;
-        passengerCount++;
+    
+    function remTrip(uint key) external restricted {
+        tripSet.remove(key); // Note that this will fail automatically if the key doesn't exist
+        delete trips[key];
+        emit LogRemTrip(msg.sender, key);
     }
-
-    function cancelTripBooking() public {
-        uint256 amount = passengers[msg.sender];
-        require(amount > 0);
-        passengers[msg.sender] = 0;
-        passengerCount--;
-        msg.sender.transfer(amount);
+    
+    function getTrip(uint key) external view returns(uint, uint, uint, uint, string memory, string memory, string memory, bytes32, bool, bool ) {
+        require(tripSet.exists(key), "Can't get a widget that doesn't exist.");
+        Trip storage trip = trips[key];
+        return (trip.tripID, trip.passengerCount, trip.paybackRatio, trip.price, trip.trainID, trip.locationSignature, trip.advertisedTimeAtLocation,trip.timeAtLocation, trip.isRefundable, trip.isActive);
     }
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
+    
+    function getTripCount() public view returns(uint count) {
+        return tripSet.count();
     }
-
-    function changePrice(uint256 _price) public restricted {
-        price = _price;
+    
+    function updateTripPrice(uint key, uint price) external restricted requireTrip(key){
+        Trip storage trip = trips[key];
+        trip.price = price;
+        emit LogUpdateTripPrice(msg.sender, key, price);
     }
-
-    function changeTrainId(string memory _trainID) public restricted {
-        trainID = _trainID;
+    
+    function bookTrip(uint key) external payable requireTrip(key){
+        Trip storage trip = trips[key];
+        require(msg.value == trip.price, "User did not pay the exact price of the trip");
+        trip.passengers[msg.sender] = trip.price;
+        trip.passengerCount++;
+        emit LogNewTripPassenger(msg.sender, key, trip.price);
     }
-
-    function changeLocationSignature(string memory _locationSignature)
-        public
-        restricted
-    {
-        locationSignature = _locationSignature;
+    
+    function cancelBooking(uint key) external {
+        Trip storage trip = trips[key];
+        require(trip.passengers[msg.sender] > 0, "User is not a passenger of this trip!");
+        trip.passengers[msg.sender] = 0;
+        trip.passengerCount--;
+        msg.sender.call.value(trip.price)("");
     }
-
-    function changeAdvertisedTimeAtLocation(
-        string memory _advertisedTimeAtLocation
-    ) public restricted {
-        advertisedTimeAtLocation = _advertisedTimeAtLocation;
-    }
-
-    //function refund() public {}
-
+    
+    // CHAINLINK FUNCTIONS
+    
     // param _requestTime must be specified in the format of a UNIX timestamp
     function requestAlarmClock(uint256 _requestTime) public restricted {
         Chainlink.Request memory req = buildChainlinkRequest(
@@ -191,7 +165,7 @@ contract Trip is ChainlinkClient {
         requestTimeAtLocation();
     }
 
-    function requestTimeAtLocation() private {
+    function requestTimeAtLocation() public {
         Chainlink.Request memory req = buildChainlinkRequest(
             JOB_ID_GET_PATH,
             address(this),
@@ -206,8 +180,10 @@ contract Trip is ChainlinkClient {
         public
         recordChainlinkFulfillment(_requestId)
     {
+        Trip storage trip = trips[activeTripKey];
+        trip.timeAtLocation = _time;
         emit RequestTimeAtLocation(_requestId, _time);
-        timeAtLocation = _time;
+        
     }
 
     function getChainlinkToken() public view returns (address) {
@@ -221,5 +197,9 @@ contract Trip is ChainlinkClient {
             "Unable to transfer"
         );
     }
-
+    
+    // TEMPORARY FUNCTIONS
+    function setActiveKey(uint key) public {
+        activeTripKey = key;
+    }
 }
