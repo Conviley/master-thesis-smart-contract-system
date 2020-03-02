f2b0f530361523505aede09f99d6e0ea31a4f976
Daniel Herzegh <danhe178@student.liu.se>
2020-03-02 13:40:33 +0100

    create adapter which compares times and returns paybackRatio

--- external_adapter/compare_times/index.js
+++ external_adapter/compare_times/index.js
@@ -2,11 +2,12 @@ var request = require('request')
 var utf8 = require('utf8')
 
 exports.retrieveTALData = (req, res) => {
-  const url = 'https://api.trafikinfo.trafikverket.se/v2/data.json'
+  const url = req.body.data.url || ''
   const key = '4ac7c190db5942c8bf99fa49078c5376' // TODO: Possibly change this so that the key is stored and sent from the node operator
   const trainId = req.body.data.advertisedTrainIdent || ''
   const locationSig = req.body.data.locationSignature || ''
   const advertisedTimeAtLocation = req.body.data.advertisedTimeAtLocation || ''
+  const shortTrip = req.body.data.shortTrip || ''
 
   var trainAnnouncementReq =
     '<REQUEST>' +
@@ -39,7 +40,7 @@ exports.retrieveTALData = (req, res) => {
 
   request.post(
     {
-      url: 'https://api.trafikinfo.trafikverket.se/v2/data.json',
+      url: url,
       method: 'POST',
       headers: {
         'Content-Type': 'text/xml',
@@ -55,13 +56,44 @@ exports.retrieveTALData = (req, res) => {
         }
         res.status(response.statusCode).send(errorData)
       } else {
+        var obj = JSON.parse(body)
+        const timeAtLocation =
+          obj.RESPONSE.RESULT[0].TrainAnnouncement[0].TimeAtLocation
+        var unixAdvertisedTimeAtLocation =
+          new Date(advertisedTimeAtLocation).getTime() / 1000
+        var unixTimeAtLocation = new Date(timeAtLocation).getTime() / 1000
+        console.log(unixAdvertisedTimeAtLocation)
+        console.log(unixTimeAtLocation)
+        const delayInSec = unixTimeAtLocation - unixAdvertisedTimeAtLocation
+        console.log(delayInSec)
+        let paybackRatio = 0
+        if ((shortTrip && delayInSec >= 1200) || delayInSec >= 3600) {
+          if (shortTrip) {
+            console.log('short trip')
+            if (delayInSec >= 3600) {
+              paybackRatio = 1
+            } else if (delayInSec >= 2400) {
+              paybackRatio = 0.75
+            } else if (delayInSec >= 1200) {
+              paybackRatio = 0.5
+            }
+          } else {
+            console.log('long trip')
+            if (delayInSec >= 7200) {
+              paybackRatio = 0.5
+            } else if (delayInSec >= 3600) {
+              paybackRatio = 0.25
+            }
+          }
+        }
+
         let returnData = {
           jobRunID: req.body.id,
-          data: body,
+          data: paybackRatio,
         }
         console.log(returnData)
         res.status(response.statusCode).send(returnData)
       }
-    },
+    }
   )
 }
