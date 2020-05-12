07fbf1e8fc891065ae80a5ab69df302450407124
Tjelvar Guo <=>
2020-05-12 16:40:04 +0200

    box plot imporve readability

--- client/cl-interaction/plotTimeElapsed.js
+++ client/cl-interaction/plotTimeElapsed.js
@@ -3,37 +3,45 @@ const plotlib = require('nodeplotlib')
 function plotTimeElapsed(OUTPUT_FILE_PATH, titleText) {
   const rawData = require(OUTPUT_FILE_PATH)
 
-  var trace1 = {
-    x: [],
-    y: [],
-    name: 'Time Used',
-    type: 'box',
-    marker: {
-      color: 'rgb(8,81,156)',
-      outliercolor: 'rgba(219, 64, 82, 0.6)',
-      line: {
-        outliercolor: 'rgba(219, 64, 82, 1.0)',
-        outlierwidth: 2,
-      },
-    },
-    boxpoints: 'suspectedoutliers',
-
-    boxmean: true,
-  }
-
+  var data = []
+  var i = 0
   for (var key in rawData) {
-    rawData[key]['transactions'].forEach((tx) => {
-      trace1.x.push(key)
-      trace1.y.push(tx.elapsedTime)
-    })
+    if (key < 310) {
+      var trace = {
+        y: [],
+        name: key + ' Txs',
+        type: 'box',
+        marker: {
+          color: 'rgb(8,81,156)',
+          outliercolor: 'rgba(219, 64, 82, 0.6)',
+          line: {
+            outliercolor: 'rgba(219, 64, 82, 1.0)',
+            outlierwidth: 2,
+          },
+        },
+        width: 0.5,
+        boxgap: 0,
+        boxgroupgap: 0,
+        boxpoints: 'suspectedoutliers',
+        boxmean: true,
+      }
+      rawData[key]['transactions'].forEach((tx) => {
+        if (tx.elapsedTime < 600) {
+          trace.y.push(tx.elapsedTime)
+        }
+      })
+      data.push(trace)
+    }
   }
-  var data = [trace1]
+
+  //var data = [trace1]
   const layout = {
     title: {
       text: titleText,
       xref: 'paper',
       x: 0.05,
     },
+    height: 900,
     xaxis: {
       title: {
         text: 'Number of Transactions',
