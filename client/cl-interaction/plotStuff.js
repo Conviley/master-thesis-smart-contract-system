578d8decd58e2990122473ec4ea8fd83614f76e4
Daniel Herzegh <danhe178@student.liu.se>
2020-05-28 14:57:26 +0200

    change commenting in plotStuff

--- client/cl-interaction/plotStuff.js
+++ client/cl-interaction/plotStuff.js
@@ -12,14 +12,14 @@ const OUTPUT_MERGED_FILE_PATH = './p1Merged.json'
 const TRAFIKVERKET_OUTPUT_FILE_PATH = './CToutput.json'
 
 function plotStuff() {
-  /* plotGasUsage(SUBMISSIONS_OUTPUT_FILE_PATH, 'Gas Usage for Submissions')
+  plotGasUsage(SUBMISSIONS_OUTPUT_FILE_PATH, 'Gas Usage for Submissions')
   plotBlockDelayBox(SUBMISSIONS_OUTPUT_FILE_PATH, 'Block Delay for Submissions')
   plotTimeElapsed(
     SUBMISSIONS_OUTPUT_FILE_PATH,
     'Network latency for Submissions'
-  ) */
+  )
 
-  /*   plotGasUsage(AGGREGATIONS_OUTPUT_FILE_PATH, 'GAS Usage for Aggregations')
+  plotGasUsage(AGGREGATIONS_OUTPUT_FILE_PATH, 'GAS Usage for Aggregations')
   plotBlockDelayBox(
     AGGREGATIONS_OUTPUT_FILE_PATH,
     'Block Delay for Aggregations'
@@ -27,15 +27,15 @@ function plotStuff() {
   plotTimeElapsed(
     AGGREGATIONS_OUTPUT_FILE_PATH,
     'Network Latency for Aggregations'
-  ) */
+  )
 
-  /* plotGasUsage(OUTPUT_MERGED_FILE_PATH, 'GAS Usage - Total')
+  plotGasUsage(OUTPUT_MERGED_FILE_PATH, 'GAS Usage - Total')
   plotBlockDelayBox(OUTPUT_MERGED_FILE_PATH, 'Block Delay - Total')
-  plotTimeElapsed(OUTPUT_MERGED_FILE_PATH, 'Network Latency - Total') */
+  plotTimeElapsed(OUTPUT_MERGED_FILE_PATH, 'Network Latency - Total')
 
-  plotGasUsage(TRAFIKVERKET_OUTPUT_FILE_PATH, 'GAS Usage - Total')
+  /*   plotGasUsage(TRAFIKVERKET_OUTPUT_FILE_PATH, 'GAS Usage - Total')
   plotBlockDelayBox(TRAFIKVERKET_OUTPUT_FILE_PATH, 'Block Delay - Total')
-  plotTimeElapsed(TRAFIKVERKET_OUTPUT_FILE_PATH, 'Network Latency - Total')
+  plotTimeElapsed(TRAFIKVERKET_OUTPUT_FILE_PATH, 'Network Latency - Total') */
 
   console.log('Plot finished!')
   //await new Promise((r) => setTimeout(r, 3000))
